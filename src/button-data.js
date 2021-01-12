62cd65dd3709ce6fff023c2cfa30b84e3830bc66
Alfred021 <alfredomedina10@hotmail.com>
2021-01-12 20:02:02 -0300

    Added app funcionality and making css responsive

---
+++ src/button-data.js
@@ -0,0 +1,19 @@
+const numbers = [
+    [
+    {value: 1, id: "one"}, {value: 2, id: "two"}, {value: 3, id: "three"}, {name: "+", id: "add"}
+    ], 
+    [
+    {value: 4, id: "four"}, {value: 5, id: "five"}, {value: 6, id: "six"}, {name: "-", id: "subtract"}
+    ], 
+    [
+    {value: 7, id: "seven"}, {value: 8, id: "eight"}, {value: 9, id: "nine"}, {name: "*", id: "multiply"}
+    ],
+    [
+    {value: 0, id: "zero"}, {name: ".", id:"decimal"}, {name:"=", id: "equals"}, {name: "/", id:"divide"}
+    ],
+    [
+      {name: "AC", id:"clear"}, {name: "CE", id: "clear-last"}
+    ]
+    ];
+
+export default numbers    
